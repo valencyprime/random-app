929f6690ddab60e8154d4c6d719a76f6f43d5022
valencyprime <valnvalentine10@gmail.com>
2023-05-20 17:15:03 +0100

    first commit

---
+++ src/Dashboard/Components/Footer.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function Footer() {
+  return (
+    <div id="footer">
+      <span>by</span>
+      <a rel="noopener norefrrer" href="#">
+        ⭐Val Valentine⭐
+      </a>
+    </div>
+  );
+}
+
+export default Footer;
